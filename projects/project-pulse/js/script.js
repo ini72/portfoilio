e8b377e9fdb18d2746a4bd5df00e49b664f4ef71
ini72 <ini77772@gmail.com>
2023-02-02 12:56:06 +0500

    fix file paths (pulse)

--- projects/project-pulse/js/script.js
+++ projects/project-pulse/js/script.js
@@ -4,8 +4,8 @@ $(document).ready(function(){
 
     $('.carousel__inner').slick({
         speed: 1200,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/arrow-left.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../icons/arrow-right.png"></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrow-left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/arrow-right.png"></button>',
         responsive: [
             {
                 breakpoint: 992,
